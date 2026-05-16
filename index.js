6cf1d502352aa0f01ba0ef38c995963d2409e432
Hammad <hammadnawaz519@gmail.com>
2026-05-16 21:02:33 +0500

    Listen on 0.0.0.0

--- index.js
+++ index.js
@@ -176,6 +176,6 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3001;
-httpServer.listen(PORT, () => {
+httpServer.listen(PORT, '0.0.0.0', () => {
   console.log(`Socket.io server listening on port ${PORT}`);
 });
